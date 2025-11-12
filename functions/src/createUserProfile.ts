5a3cb16f240e31d2c0931779c511a0b8e3df7c47
SergeyKhval <sergeykhval@gmail.com>
2025-11-12 15:36:01 +0100

    feat(app): automatically add contact to resend

--- functions/src/createUserProfile.ts
+++ functions/src/createUserProfile.ts
@@ -2,13 +2,34 @@ import { user } from "firebase-functions/v1/auth";
 import { defineString } from "firebase-functions/params";
 import { FieldValue, getFirestore } from "firebase-admin/firestore";
 import Stripe from "stripe";
+import { Resend } from "resend";
 
 const STRIPE_API_KEY = defineString("STRIPE_API_KEY");
+const RESEND_API_KEY = defineString("RESEND_API_KEY");
 
 const db = getFirestore();
 
 export const createUserProfile = user().onCreate(async (user) => {
   const stripeClient = new Stripe(STRIPE_API_KEY.value());
+  const resendClient = new Resend(RESEND_API_KEY.value());
+
+  if (user.email) {
+    const payload = {
+      audienceId: "0378a299-a329-4239-944d-c8b787329e76",
+      email: user.email,
+      firstName: "",
+      lastName: "",
+    };
+    const [firstName, lastName] = user.displayName?.split(" ") || ["", ""];
+    payload.firstName = firstName;
+    payload.lastName = lastName;
+
+    try {
+      await resendClient.contacts.create(payload);
+    } catch (e) {
+      console.error("Error adding contact to Resend:", e);
+    }
+  }
 
   try {
     const customer = await stripeClient.customers.create({ email: user.email });
